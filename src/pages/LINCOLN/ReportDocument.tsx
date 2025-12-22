b87b50ab74eb061c57ade9a9999a317a9ea76f74
dikie001 <omondidickens255@gmail.com>
2025-12-22 17:13:38 +0300

    updated lincoln's image

--- src/pages/LINCOLN/ReportDocument.tsx
+++ src/pages/LINCOLN/ReportDocument.tsx
@@ -399,7 +399,7 @@ export const ReportDocument = ({
           <View style={styles.headerContent}>
             <View style={styles.userInfoSection}>
               <View style={styles.avatarContainer}>
-                <Image src="/images/logo.png" style={styles.avatarImage} />
+                <Image src="/images/lincoln.png" style={styles.avatarImage} />
               </View>
               <View style={styles.userDetails}>
                 <Text style={styles.headerName}>{userName}</Text>
